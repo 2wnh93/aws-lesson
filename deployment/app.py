25e10850c8df9a55d0f770f70c38b7f744251d62
mrivantan <mr.ivan.tan@gmail.com>
2021-02-04 04:16:55 +0800

    added cors

--- deployment/app.py
+++ deployment/app.py
@@ -1,33 +1,14 @@
-# Loads Flask Library
 from flask import Flask
-# initialises a flask application
-app = Flask(__name__)
-
-# used to serve html files
-# files have to be inside "templates" folder
-from flask import render_template
-
-# handles REST API requests
 from flask import request
-
-# loads our model
+from flask_cors import CORS
 import pickle
 
+app = Flask(__name__)
+CORS(app)
 
-# hello world test
-@app.route('/hello') # route defines the API's URI: http://localhost:5000/hello
-# function immediately after route will be executed
-def hello(): 
-    # What the flask server will return
-    return "hello world"
-
-# serve website
 @app.route('/')
 def home():
-    # notice that "./index.html" actually means "templates/index.html"
-    # this is because render_template() treats 'templates' folder as the root
-    return render_template("./index.html")
-
+    return "Try: http://<YOUR AWS PUBLIC URL>/api/diabetes?x=0,1,2,3,4,5,6,7,8,9"
 
 # load the model
 filename = 'finalized_model.sav'
@@ -35,7 +16,7 @@ with open(filename, 'rb') as file:
     model = pickle.load(file)
 
 # predicts diabetes
-# expected uri: http://localhost:5000/api/diabetes?x=0,1,2,3,4,5,6,7,8,9
+# expected uri: http://<YOUR AWS PUBLIC URL>/api/diabetes?x=0,1,2,3,4,5,6,7,8,9
 @app.route('/api/diabetes')
 def diabetes():
     # get the parameter named "x"
