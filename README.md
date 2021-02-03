2d2c8a662b3cecc736c93e09f6012a09921e8b9d
mrivantan <mr.ivan.tan@gmail.com>
2021-02-04 04:10:25 +0800

    testing with html

--- README.md
+++ README.md
@@ -5,3 +5,33 @@
 git clone https://github.com/mrivantan/aws-lesson.git
 ```
 
+
+
+```
+sudo apt update
+sudo apt install python3-pip git tmux
+```
+
+
+```
+git clone https://github.com/mrivantan/aws-lesson.git
+cd aws-lesson
+```
+
+```
+pip3 install virtualenv
+python3 -m virtualenv -p python3 env
+source env/bin/activate
+```
+
+```
+pip install -r requirements.txt
+```
+
+```
+tmux new -s app_session
+```
+
+```
+tmux ls
+```
--- deployment/app.py
+++ deployment/app.py
@@ -1,12 +1,33 @@
+# Loads Flask Library
 from flask import Flask
-from flask import request
-import pickle
-
+# initialises a flask application
 app = Flask(__name__)
 
+# used to serve html files
+# files have to be inside "templates" folder
+from flask import render_template
+
+# handles REST API requests
+from flask import request
+
+# loads our model
+import pickle
+
+
+# hello world test
+@app.route('/hello') # route defines the API's URI: http://localhost:5000/hello
+# function immediately after route will be executed
+def hello(): 
+    # What the flask server will return
+    return "hello world"
+
+# serve website
 @app.route('/')
 def home():
-    return "Try: http://<YOUR AWS PUBLIC URL>/api/diabetes?x=0,1,2,3,4,5,6,7,8,9"
+    # notice that "./index.html" actually means "templates/index.html"
+    # this is because render_template() treats 'templates' folder as the root
+    return render_template("./index.html")
+
 
 # load the model
 filename = 'finalized_model.sav'
@@ -14,7 +35,7 @@ with open(filename, 'rb') as file:
     model = pickle.load(file)
 
 # predicts diabetes
-# expected uri: http://<YOUR AWS PUBLIC URL>/api/diabetes?x=0,1,2,3,4,5,6,7,8,9
+# expected uri: http://localhost:5000/api/diabetes?x=0,1,2,3,4,5,6,7,8,9
 @app.route('/api/diabetes')
 def diabetes():
     # get the parameter named "x"
